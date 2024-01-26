83067f63f9d6b7bd16063857a65e4bb12cfc6dd8
VoxanyNet <contact@voxany.net>
2024-01-25 21:08:28 -0500

    open config in read mode

--- coolvoxanygames_updater/__main__.py
+++ coolvoxanygames_updater/__main__.py
@@ -7,7 +7,7 @@ DISCORD_TOKEN = os.environ["COOLVOXANYGAMES_UPDATER_BOT_DISCORD_TOKEN"]
 GAMES_PATH = os.environ["COOLVOXANYGAMES_UPDATER_GAMES_FOLDER"]
 CONFIG_DIRECTORY = os.environ["COOLVOXANYGAMES_UPDATER_CONFIG_DIRECTORY"]
 
-with open(f"{CONFIG_DIRECTORY}/owned_games.json", "w") as file:
+with open(f"{CONFIG_DIRECTORY}/owned_games.json", "r") as file:
     owned_games = json.load(file)
 
 bot = bot.CoolVoxanyGamesUpdater(games_folder=GAMES_PATH, owned_games=owned_games)

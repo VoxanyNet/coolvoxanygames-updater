07b7ef2b4717dee383d30c464bebc4de3a53c458
VoxanyNet <contact@voxany.net>
2024-01-25 21:32:26 -0500

    replace spaces with %20 in url

--- coolvoxanygames_updater/commands.py
+++ coolvoxanygames_updater/commands.py
@@ -28,4 +28,4 @@ async def update(
 
         return
 
-    await ctx.respond(f"**Update complete!** ✅ \n\n[Play it!](https://games.voxany.net/games/{game}/index.html)", ephemeral=True)
+    await ctx.respond(f"**Update complete!** ✅ \n\n[Play it!](https://games.voxany.net/games/{game}/index.html)".replace(" ", "%20"), ephemeral=True)

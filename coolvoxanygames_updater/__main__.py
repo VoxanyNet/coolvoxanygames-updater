2bfbee5a5a7e4c138f30815017c1392833b67dc0
VoxanyNet <contact@voxany.net>
2024-01-25 21:05:19 -0500

    fixed typo in config env var

--- coolvoxanygames_updater/__main__.py
+++ coolvoxanygames_updater/__main__.py
@@ -5,7 +5,7 @@ from coolvoxanygames_updater import bot
 
 DISCORD_TOKEN = os.environ["COOLVOXANYGAMES_UPDATER_BOT_DISCORD_TOKEN"]
 GAMES_PATH = os.environ["COOLVOXANYGAMES_UPDATER_GAMES_FOLDER"]
-CONFIG_DIRECTORY = os.environ["COOLVOXANYGAMES_CONFIG_DIRECTORY"]
+CONFIG_DIRECTORY = os.environ["COOLVOXANYGAMES_UPDATER_CONFIG_DIRECTORY"]
 
 with open(f"{CONFIG_DIRECTORY}/owned_games.json", "w") as file:
     owned_games = json.load(file)
